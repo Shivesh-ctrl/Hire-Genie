6538c1c6e8dbbae534b15730ab2fe71eb23059bd
Shivesh-ctrl <shiveshsriv@gmail.com>
2025-12-05 16:16:59 +0530

    Add files via upload

--- (main)/layout.js
+++ (main)/layout.js
@@ -1,9 +1,48 @@
-import React from 'react'
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { ThemeProvider } from "@/components/ui/theme-provider";
+import Header from "@/components/ui/header";
+import { ClerkProvider } from "@clerk/nextjs";
+import { dark } from '@clerk/themes'
+import { Toaster } from "sonner";
 
-const MainLayout = ({children}) => {
-  //redirect to onboarding
-  return <div className="container mx-auto mt-24 mb-20">{children}</div>
-  
-}
+const inter = Inter({ subsets: ["latin"] });
 
-export default MainLayout;
+export const metadata = {
+  title: "HireGenie - AI Powered Career Guide",
+  description: "Job preparation made easy",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <ClerkProvider
+      appearance={{
+        baseTheme: dark,
+      }}
+    >
+    <html lang="en" suppressHydrationWarning>
+      <body
+        className={`${inter.className}`}
+      >
+        <ThemeProvider
+            attribute="class"
+            defaultTheme="dark"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {/* header */}
+            <Header />
+            <main className="min-h-screen">{children}</main>
+            <Toaster richColors />
+            {/* footer */}
+            <footer className="bg-muted/50 py-12"> 
+              <div className="container mx-auto px-4 text-center text-gray-200">
+                <p>Made with ♥ by RuhaniSingh</p>
+              </div>
+            </footer>
+          </ThemeProvider>
+      </body>
+    </html>
+    </ClerkProvider>
+  );
+}   
